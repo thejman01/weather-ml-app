698c408f418519bfdab951d8789595593daf852d
thejman01 <scottkelly1245@yahoo.com>
2025-12-03 23:45:46 +0000

    Changing Foggy Test To Cloudy Test

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -2,19 +2,22 @@ import unittest
 from app import app
 
 class TestAppSmoke(unittest.TestCase):
-	def setUp(self):
-		app.testing = True
-		self.client = app.test_client()
-	
-	# Complete the function below to test a success in running the application
-	def test_prediction_route_success(self):
-		response = self.client.get('/')
-		
+    def setUp(self):
+        app.testing = True
+        self.client = app.test_client()
+    
+    # Complete the function below to test a success in running the application
+    def test_prediction_route_success(self):
+        response = self.client.get('/')
+         # 200 = OK, confirms the Flask app is running and route is configured
+        self.assertEqual(response.status_code, 200)
 
-	# Complete the function below to test a form is rendered
-	def test_get_form(self):
-		response = self.client.get('/')
-		
+    # Complete the function below to test a form is rendered
+    def test_get_form(self):
+        response = self.client.get('/')
+        # Basic smoke check that a form is present in the HTML
+        self.assertIn(b'<form', response.data)
+        
  
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
